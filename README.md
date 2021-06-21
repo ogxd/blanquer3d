a7a1dd2df6591efed4a28096f44f4ba077e6c062
Olivier Giniaux <oginiaux@gmail.com>
2021-06-21 23:19:03 +0200

    Add hierarchy add button

---
+++ README.md
@@ -0,0 +1,3 @@
+# Blanquer 3D
+
+![blanquer3d](demo/blanquer3d.png)
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import Hierarchy from "./components/hierarchy";
+//import "bootstrap/dist/css/bootstrap.css";
+
+ReactDOM.render(<Hierarchy />, document.getElementById("root"));
