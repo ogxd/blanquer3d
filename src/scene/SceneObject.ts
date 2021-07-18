541eac6a59bcf6f9340516b9ee7d2dc3fa34cc27
Olivier Giniaux <oginiaux@gmail.com>
2021-07-18 10:58:46 +0200

    Fix missing reference

--- src/scene/SceneObject.ts
+++ src/scene/SceneObject.ts
@@ -1,9 +1,7 @@
-import { Serializable, JsonProperty } from "typescript-json-serializer";
 import EventSubscriber from "src/core/EventSubscriber";
 import { property } from "src/core/PropertyDecorator";
 import { ISerializable } from "src/core/Serialization";
 
-@Serializable()
 abstract class SceneObject implements ISerializable {
   serialize(object: any) {
     object["visibility"] = this.visibility;
